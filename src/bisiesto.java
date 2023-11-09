d4610b4f537d110d33877982b44ab05a6ea41e38
Basileia6 <beatrizvd6@gmail.com>
2023-11-09 14:43:20 +0100

    Add class bisiesto

---
+++ src/bisiesto.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class bisiesto {
+
+    public static void main (String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Introduzca un año");
+        int num1 = reader.nextInt();
+        int resto = num1 % 4;
+
+        if (resto == 0 ){
+            System.out.println("El año es bisiesto");
+        } else {
+            System.out.println("El año no es bisiesto");
+        }
+    }
+}
