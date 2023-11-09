b3024c53fa9c5187bf17fdbba647f9e3ac1da8e9
Basileia6 <beatrizvd6@gmail.com>
2023-11-09 20:01:10 +0100

    Add class numconsecutivos

---
+++ src/numconsecutivos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class numconsecutivos {
+
+    public static void main (String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Introduzca un número");
+        int primero = reader.nextInt();
+
+        System.out.println("Introduzca otro número");
+        int segundo = reader.nextInt();
+
+        System.out.println("Introduzca otro número más");
+        int tercero = reader.nextInt();
+
+        if (segundo != primero + 1 ){
+            System.out.println("Los números no son consecutivos");
+        } else if (tercero != segundo + 1) {
+            System.out.println("Los números no son consecutivos");
+        } else {
+            System.out.println("Los números son consecutivos");
+        }
+    }
+}
