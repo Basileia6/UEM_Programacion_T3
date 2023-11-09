c9ad278746399388edfd4c0dbc0c7c7a140b2c33
Basileia6 <beatrizvd6@gmail.com>
2023-11-09 14:18:18 +0100

    Add class compare2nums

---
+++ src/compare2nums.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class compare2nums {
+
+    public static void main (String[] args){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Introduzca un número ");
+        int num1 = reader.nextInt();
+
+        System.out.println("Introduzca otro número ");
+        int num2 = reader.nextInt();
+
+        if (num1 > num2){
+            System.out.println("El número 1 es mayor que el número 2 ");
+        } else if (num1 < num2) {
+            System.out.println("El número 1 es menor que el número 2 ");
+        } else {
+            System.out.println("El número 1 y número 2 son iguales");
+        }
+    }
+}
