7cf83a43c6de4e3808a6446f47beeb8456e7d236
Basileia6 <beatrizvd6@gmail.com>
2023-11-09 20:05:23 +0100

    Add class paroimpar

---
+++ src/paroimpar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class paroimpar {
+
+    public static void main (String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Introduzca un número");
+        int num = reader.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num + 1);
+        } else {
+            System.out.println(num - 1);
+        }
+    }
+
+}
