ecc8848a6ab359bb1afad24e1b851d7cf3f1d6d3
Basileia6 <149323724+Basileia6@users.noreply.github.com>
2023-11-10 23:35:19 +0100

    Fix

---
+++ src/t3_FOR_ej01_mediatemp (2).java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class t3_FOR_ej01_mediatemp {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Introduce el número de temperaturas a leer");
+		int num = reader.nextInt();
+		double sumatemp = 0.0;
+		
+		if (num<=0){
+		    num = 10;
+		}
+		
+		
+		for (int i=1;i<=num;i++){
+		    System.out.println("Introduzca una temperatura");
+		    sumatemp += reader.nextDouble();
+		}
+		
+		double result = sumatemp/num;
+		
+		System.out.println("La media de las temperaturas introducidas es: "+result);
+	}
+}
